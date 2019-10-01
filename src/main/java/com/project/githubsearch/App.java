0762518f30955fd09702890c4b24b02840253796
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2019-10-02 06:53:03 +0700

    retrieve more result using pagination

--- src/main/java/com/project/githubsearch/App.java
+++ src/main/java/com/project/githubsearch/App.java
@@ -3,6 +3,7 @@ package com.project.githubsearch;
 import com.github.kevinsawicki.http.HttpRequest;
 import org.json.JSONObject;
 import org.json.JSONArray;
+import org.json.JSONException;
 
 /**
  * Github Search Engine
@@ -11,38 +12,52 @@ import org.json.JSONArray;
 public class App {
     public static void main(String[] args) {
 
-        String OAUTH_TOKEN = "48b6674f0e050c99bc6418947b6a8950b6893c9b";
+        String OAUTH_TOKEN = "39bcdebc4dc9353757acf13dac3708b5460a5e80";
         int BAD_CREDENTIAL = 401;
         int RESPONSE_OK = 200;
 
-        String url = "https://api.github.com/search/code";
+        String endpoint = "https://api.github.com/search/code";
         String query = "java.lang.String";
 
-        HttpRequest request = HttpRequest.get(url, true, 'q', query, "in", "file", "language", "java")
-                .authorization("token " + OAUTH_TOKEN);
+        int per_page_limit = 100;
+        int page = 0;
+        
+        String url = endpoint + "?q=" + query + "+in:file+language:java+extension:java+path:src/&page=" + page + "&per_page=" + per_page_limit;
+        
+        HttpRequest request = HttpRequest.get(url).authorization("token " + OAUTH_TOKEN)
+        .accept("application/vnd.github.v3.text-match+json");
+        
         int responseCode = request.code();
         String responseBody = request.body();
-        JSONObject response = new JSONObject(responseBody);
-
-        if (responseCode == RESPONSE_OK) {
-
-            JSONArray items = response.getJSONArray("items");
-            if (items.length() > 0) {
-                System.out.println("Total result: " + response.getInt("total_count"));
-                JSONObject item = items.getJSONObject(0); // get the first item
-                System.out.println("Print First Item ");
-                System.out.println("Repository: " + item.getJSONObject("repository").getString("full_name"));
-                System.out.println("Filename: " + item.getString("name"));
-                System.out.println("File url: " + item.getString("html_url"));
-            } else {
-                System.out.println("There is no result for the query");
-            }
+        JSONObject queryResult = new JSONObject(responseBody);
+        
+        int i = 0;
+        for (i = 0; i < 10; i++) {
+            page = i;
+            url = endpoint + "?q=" + query + "+in:file+language:java+extension:java+path:src/&page=" + page + "&per_page=" + per_page_limit;
+    
+            request = HttpRequest.get(url)
+                    .authorization("token " + OAUTH_TOKEN)
+                    .accept("application/vnd.github.v3.text-match+json");
+                    
+            responseCode = request.code();
+            responseBody = request.body();
+            JSONObject response = new JSONObject(responseBody);
             
-        } else if (responseCode == BAD_CREDENTIAL) {
-            System.out.println(response.getString("message"));
-            System.out.println("Authorization problem");
-            System.out.println("Please read the readme file!");
+            if (responseCode == RESPONSE_OK) {
+                JSONArray item = response.getJSONArray("items");
+                JSONArray items = queryResult.getJSONArray("items");
+                for (int j = 0; j < item.length(); j++) {
+                    items.put(item.get(j));
+                }
+            } else if (responseCode == BAD_CREDENTIAL) {
+                System.out.println(response.getString("message"));
+                System.out.println("Authorization problem");
+                System.out.println("Please read the readme file!");
+            }
         }
+
+        System.out.println(queryResult.toString());
 
     }
 
