56cd4c8d755508f8d10c785b130ee7e4c0e90d8a
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2019-10-10 17:39:26 +0700

    implement configurable size interval and flexible page limit

--- src/main/java/com/project/githubsearch/App.java
+++ src/main/java/com/project/githubsearch/App.java
@@ -16,6 +16,7 @@ import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -26,7 +27,8 @@ import java.util.stream.Collectors;
  */
 public class App {
 
-    private static final String AUTH_TOKEN = "5a2446f46bc95d4b915ff8e14bc0efac1827072e";
+    private static final String AUTH_TOKEN = System.getenv("GITHUB_AUTH_TOKEN");
+    // private static final String AUTH_TOKEN = "5a2446f46bc95d4b915ff8e14bc0efac1827072e";
     // parameter for the request
     private static String PARAM_QUERY = "q"; //$NON-NLS-1$
     private static String PARAM_PAGE = "page"; //$NON-NLS-1$
@@ -34,13 +36,9 @@ public class App {
     
     private static String ACCEPT_TEXT_MATCH = "application/vnd.github.v3.text-match+json";
     
-    // some links from the response header
+    // links from the response header
     private static String META_REL = "rel"; //$NON-NLS-1$
-    private static String META_LAST = "last"; //$NON-NLS-1$
     private static String META_NEXT = "next"; //$NON-NLS-1$
-    private static String META_FIRST = "first"; //$NON-NLS-1$
-    private static String META_PREV = "prev"; //$NON-NLS-1$
-    
     private static final String DELIM_LINKS = ","; //$NON-NLS-1$
     private static final String DELIM_LINK_PARAM = ";"; //$NON-NLS-1$
         
@@ -126,50 +124,71 @@ public class App {
     private static void getData() throws InterruptedException, IOException {
         String endpoint = "https://api.github.com/search/code";
         String query = "java.lang.String.replaceAll";
-        // String[] paths = { "app/src/", "src/", "lib/" };
-        String[] sizes = { "10000..50000", "<10000", ">50000" };
+
+        ArrayList<String> sizes = new ArrayList<String>();
+        // int MAX_SIZE = 1000; 
+        int MAX_SIZE = 384000; // the max searchable size from github api
+        int INTERVAL = 2000;
+        String range = "";
+        int lower_bound = 0;
+        int upper_bound = lower_bound + INTERVAL;
+        while (lower_bound <= MAX_SIZE) {
+            upper_bound = lower_bound + INTERVAL;
+            range =  lower_bound + ".." + upper_bound;
+            lower_bound = upper_bound;
+            sizes.add(new String(range));
+        }
         
 
         JSONArray result = new JSONArray();
 
         int per_page_limit = 100;
         int page = 1;
-        int last_page = 2;
-
+        
         System.out.println("Getting data from Github");
         System.out.println("with per page limit: " + per_page_limit);
-        int id = 1;
+        
+        for (int i = 0; i < sizes.size(); i++) { 
+            String size = sizes.get(i);
+            
+            String next = null;
+            String url = null;
+            
+            do {
+                if (url == null) {
+                    url = endpoint + "?" 
+                                    + PARAM_QUERY + "=" + query + "+in:file+language:java+extension:java+size:" + size
+                                    + "&" + PARAM_PAGE + "=" + page
+                                    + "&" + PARAM_PER_PAGE + "=" + per_page_limit;
+                }
 
-        for (int i = 0; i < sizes.length; i++) { 
-            String size = sizes[i];
-            for (int j = 1; j <= last_page; j++) { // the page start at 1
-                page = j;
+                HttpRequest request = HttpRequest.get(url, false)
+                                                    .authorization("token " + AUTH_TOKEN);
+                                                    // .accept(ACCEPT_TEXT_MATCH);
+
                 System.out.println("");
-                System.out.println("Iteration number: " + id++);
-                System.out.println("Size: " + size);
-                System.out.println("Page: " + page);
-                
-                HttpRequest request = HttpRequest.get(endpoint, false
-                                                        , PARAM_QUERY, query + "+in:file+language:java+extension:java+size:" + size
-                                                        , PARAM_PAGE, page
-                                                        , PARAM_PER_PAGE, per_page_limit)
-                                                .authorization("token " + AUTH_TOKEN)
-                                                .accept(ACCEPT_TEXT_MATCH);
                 System.out.println("Request: " + request);
-                
+
                 // handle response
                 int responseCode = request.code();
                 if (responseCode == RESPONSE_OK) {
                     // System.out.println("Response Headers: " + request.headers());
-                    System.out.println("Link: " + request.header("Link"));
-                    System.out.println("Next: " + getNextLinkFromResponse(request.header("Link")));
                     String responseBody = request.body();
                     JSONObject response = new JSONObject(responseBody);
                     JSONArray item = response.getJSONArray("items");
-                    System.out.println("Total items in current request: " + item.length());
+                    System.out.println("Number items in current request: " + item.length());
                     for (int it = 0; it < item.length(); it++) {
-                        result.put(item.get(it));
+                        JSONObject instance = new JSONObject(item.get(it).toString());
+                        JSONObject obj = new JSONObject();
+                        obj.put("html_url", instance.getString("html_url"));
+                        obj.put("name", instance.getString("name"));
+                        result.put(obj);
                     }
+                    next = getNextLinkFromResponse(request.header("Link"));
+                    if (next == null) {
+                        System.out.println("This is the last page");
+                    }
+                    url = next;
                 } else if (responseCode == BAD_CREDENTIAL) {
                     System.out.println("Authorization problem");
                     System.out.println("Please read the readme file!");
@@ -177,22 +196,31 @@ public class App {
                     System.exit(-1);
                 } else if (responseCode == ABUSE_RATE_LIMITS) {
                     System.out.println("Abuse Rate Limits");
-                    System.out.println("Retry-After: " + request.header("Retry-After"));
+                    // retry current progress after wait for some seconds
+                    String retryAfter = request.header("Retry-After");
                     try {
-                        int sleepTime = 60; // wait for 60 seconds
+                        int sleepTime = 0; // wait for a while
+                        if (retryAfter.isEmpty()) {
+                            sleepTime = 1;
+                        } else {
+                            sleepTime = new Integer(retryAfter).intValue();
+                        }
+                        System.out.println("Retry-After: " + sleepTime);
                         TimeUnit.SECONDS.sleep(sleepTime);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    j--; // retry current progress
                 } else {
                     System.out.println("Response Code: " + responseCode);
                     System.out.println("Response Body: " + request.body());
                     System.out.println("Response Headers: " + request.headers());
+                    System.exit(-1);
                 }
-            }
+
+            } while (url != null);
         }
 
+        System.out.println("");
         System.out.println("Total items for all requests: " + result.length());
 
         // Get the file reference
